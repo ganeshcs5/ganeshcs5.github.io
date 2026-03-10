90526c3ca9d1418e579a53e3441512f7adad7bbb
Ganesh Chandrasekhara <ganesh@lemon.app>
2026-03-10 21:17:09 +0530

    Fix setTimeout type casting

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -88,7 +88,7 @@ const Scene = () => {
           element?.addEventListener("touchmove", (e: TouchEvent) =>
             handleTouchMove(e, (x, y) => (mouse = { x, y }))
           );
-        }, 200);
+        }, 200) as number;
       };
 
       const onTouchEnd = () => {
